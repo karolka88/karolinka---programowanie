fd31cfe81e96d915e043dadf099c046433e8265b
Karolina Żurawska <karolina.zurawska88@gmail.com>
2023-02-11 11:33:27 +0100

    :)

---
+++ for loops.py
@@ -0,0 +1,26 @@
+
+#for x in (range(1, 11)): #odliczanie
+#    print(x)
+
+#for x in reversed(range(1, 11)): #odwrócone odliczanie
+#    print(x)
+
+#for x in (range(1, 11, 2)): #odliczanie co 2 liczba
+#    print(x)
+
+#credit_card = "1234-5678-9012-3456"
+
+#for x in credit_card:
+#    print(x)
+
+#for x in range(1, 21): 
+#    if x == 13:        #pominięcie liczby
+#        continue
+#    else:
+#        print(x)
+
+#for x in range(1, 21): 
+#    if x == 13:        #ukończenie przed daną liczbą
+#        break
+#    else:
+#        print(x)
---
+++ nested_loop.py
@@ -0,0 +1,19 @@
+#nested loop = A loop within another loop (outer, inner)
+#               outer loop:
+#                    inner loop:
+
+#for x in range(3):          #3-krotne powtórzenie czegoś
+#    for y in range(1, 10):
+#        print(y, end=" ")   #liczby w jednej linii oddzielone np. spacją, - , lub \n(osobne linijki)
+#    print()                 #new line
+
+#rows = int(input("Enter the number of rows: "))
+#columns = int(input("Enter the number of columns: "))
+#symbol = input("Enter a symbol to use: ")
+
+#for x in range(rows):               #liczba wierszy
+#    for y in range(columns):        #liczba kolumn
+#        print(symbol, end = "")     #czym odzielone są symbole
+#    print()                         #osobne linijki
+
+#może byc for in for, może byc for in while, moze być while in for, może być while in while
